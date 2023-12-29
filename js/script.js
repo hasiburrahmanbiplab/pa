e29540be4fac1f2055a5be7f882026c7b4b978d9
Hasibur Rahman <biplobjr29@gmail.com>
2023-12-29 06:52:05 +0600

    commit-7

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $('.counter').counterUp({
 $('.popular-slider').slick({
     slidesToShow: 5,
     slidesToScroll: 2,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 1000,
     arrows:false,
     responsive: [
@@ -60,311 +60,48 @@ $('.popular-slider').slick({
     arrows:false,
     responsive: [
       {
-        breakpoint: 2201,
+        breakpoint: 1400,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 3,
+          slidesToScroll: 2,
           infinite: true,
-          autoplay: true,
         }
       },
       {
-          breakpoint: 1601,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            // autoplay: true,
-          }
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-    ]
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+  ]
   });
   // art-culture
-  $('.explore-slide-2') .slick({
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows:false,
-    responsive: [
-      {
-        breakpoint: 2201,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          infinite: true,
-          autoplay: true,
-        }
-      },
-      {
-          breakpoint: 1601,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-          }
-        },
-    ]
-  });
-  // outdoor-slide
-  $('.explore-slide-3') .slick({
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows:false,
-    responsive: [
-      {
-        breakpoint: 2201,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          infinite: true,
-          autoplay: true,
-        }
-      },
-      {
-          breakpoint: 1601,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-    ]
-  });
-  // mountain
-  $('.explore-slide-4') .slick({
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows:false,
-    responsive: [
-      {
-        breakpoint: 2201,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          infinite: true,
-          autoplay: true,
-        }
-      },
-      {
-          breakpoint: 1601,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-    ]
-  });
-  
-  // beach-slide
-  $('.explore-slide-5') .slick({
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows:false,
-    responsive: [
-      {
-        breakpoint: 2201,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          infinite: true,
-        }
-      },
-      {
-          breakpoint: 1601,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            autoplay: true,
-          }
-        },
-    ]
-  });
 
   $('.testimonial-slide').slick({
     slidesToShow: 1,
